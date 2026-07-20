5031ececf7f6bf6bc64622dbc7b3ad4c3f1c49de
Kieu Dinh Duc <ilovemoon8217@gmail.com>
2026-07-20 22:57:13 +0700

    Migrate task state to Redux and show pending count in navbar (LO8)

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const AppNavbar = () => {
+  const pendingCount = useSelector((state) =>
+    state.tasks.items.filter((task) => task.status === 'pending').length
+  );
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
       <Container>
@@ -11,7 +16,9 @@ const AppNavbar = () => {
         <Navbar.Collapse id="main-navbar">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/feature">Tasks</Nav.Link>
+            <Nav.Link as={Link} to="/feature">
+              Tasks <Badge bg="warning" text="dark">{pendingCount}</Badge>
+            </Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
